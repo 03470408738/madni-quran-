a5b5f8edd7b2e0fb2e943b73c2d8d0427c5e7581
MUTEE ULLAH CHANNER <muteeullahchanner@github.com>
2026-07-25 10:34:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Malik from './Large splitbutton.jsx'
+import Large_splitbutton from './Large splitbutton.jsx';
+
+function App() {
+  return (
+    <div>
+        <Large_splitbutton/>
+
+    </div>
+  )
+}
+
+export default App
